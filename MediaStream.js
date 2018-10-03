c77774d45f2d811a96a2d8ed489ef5db018065b8
Michał Łaszczewski <michal@emikse.com>
2018-10-03 19:24:33 +0200

    mute support

--- MediaStream.js
+++ MediaStream.js
@@ -1,8 +1,41 @@
 import { native, request } from './native'
 
+class MediaStreamTrack {
+  constructor(mediaStream, trackId) {
+    this.enabledValue = true
+    this.mediaStream = mediaStream;
+    this.trackId = trackId;
+  }
+
+  get enabled() {
+    return this.enabledValue
+  }
+
+  set enabled(value) {
+    this.enabledValue = false
+    request({
+      type: "setTrackEnabled",
+      trackId: this.trackId,
+      enabled: value,
+      userMediaId: this.mediaStream.nativeId
+    })
+  }
+}
+
 class MediaStream {
-  constructor(nativeId) {
+  constructor(nativeId, constraints) {
     this.nativeId = nativeId
+    this.tracks = []
+    if(!constraints.hasOwnProperty('audio') || constraints.audio) {
+      this.tracks.push(new MediaStreamTrack(this, this.tracks.length))
+    }
+    if(!constraints.hasOwnProperty('video') || constraints.video) {
+      this.tracks.push(new MediaStreamTrack(this, this.tracks.length))
+    }
+  }
+
+  getTracks() {
+    return this.tracks
   }
 
   toURL() {
@@ -12,7 +45,7 @@ class MediaStream {
 
 function getUserMedia(constraints, successCb, errorCb) {
   let streamPromise = request({ type: "getUserMedia", constraints }).then(msg => {
-    return new MediaStream(msg.userMediaId)
+    return new MediaStream(msg.userMediaId, constraints)
   })
   streamPromise.then((stream) => successCb && successCb(stream)).catch((error) => errorCb && errorCb(error))
   return streamPromise
